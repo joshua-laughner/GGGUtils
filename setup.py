6c044590c76fd7b4a8d2a22f23a4de83e810857c
Joshua Laughner <jlaugh@caltech.edu>
2019-09-04 17:20:58 -0700

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GGGUtils',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/joshua-laughner/GGGUtils.git',
+    license='',
+    author='Joshua Laughner',
+    author_email='jlaugh@caltech.edu',
+    description='Ancilliary utilities to run GGG'
+)
