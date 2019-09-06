c8d6d11cf262b856c58bba017bd2f67178b98fc3
Joshua Laughner <jlaugh@caltech.edu>
2019-09-06 12:50:09 -0700

    Renamed `gggrun.py` to `i2srun.py`

--- run_ggg_utils.py
+++ run_ggg_utils.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-from gggutils import target_utils, gggrun, ggglogging
+from gggutils import target_utils, i2srun, ggglogging
 
 
 def parse_args():
@@ -13,22 +13,22 @@ def parse_args():
     target_utils.parse_tab_args(tabp)
 
     build_cfg_p = subp.add_parser('build-i2s-cfg', help='Build the config file to run I2S in bulk.')
-    gggrun.parse_build_cfg_args(build_cfg_p)
+    i2srun.parse_build_cfg_args(build_cfg_p)
 
     make_i2s_runfiles_p = subp.add_parser('make-i2s-runfiles', help='Make missing I2S run files')
-    gggrun.parse_make_i2s_runfile_args(make_i2s_runfiles_p)
+    i2srun.parse_make_i2s_runfile_args(make_i2s_runfiles_p)
 
     copy_i2s_runfiles_p = subp.add_parser('copy-tgt-i2s-runfiles', help='Copy target I2S run files to a single directory')
-    gggrun.parse_copy_i2s_target_runfiles_args(copy_i2s_runfiles_p)
+    i2srun.parse_copy_i2s_target_runfiles_args(copy_i2s_runfiles_p)
 
     link_i2s_p = subp.add_parser('link-i2s-inputs', help='Link the input files to run I2S in bulk')
-    gggrun.parse_link_i2s_args(link_i2s_p)
+    i2srun.parse_link_i2s_args(link_i2s_p)
 
     run_i2s_p = subp.add_parser('run-i2s', help='Run I2S in batch')
-    gggrun.parse_run_i2s_args(run_i2s_p)
+    i2srun.parse_run_i2s_args(run_i2s_p)
 
     halt_i2s_p = subp.add_parser('halt-i2s', help='Halt an active batch I2S run')
-    gggrun.parse_halt_i2s_args(halt_i2s_p)
+    i2srun.parse_halt_i2s_args(halt_i2s_p)
 
     args = vars(p.parse_args())
     return args
