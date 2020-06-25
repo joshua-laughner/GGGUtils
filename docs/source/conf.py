7fdb0c2f24383a7bd6102d13eaecbf73db96db91
Joshua Laughner <jllacct119@gmail.com>
2020-06-25 16:57:43 -0700

    Added extensions and .nojekyll file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,13 @@ release = '0.2.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.graphviz'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
